84d17045506e35c1226c7ec988e80068580a4e1f
someshpradeep31 <61006387+someshpradeep31@users.noreply.github.com>
2020-08-19 12:18:25 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,19 @@
+class Add{
+int a,b,c;
+Add(int x,int y,int z){
+a=x;
+b=y;
+c=z;
+}
+void display(){
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}
+class Main(){
+public static void main(String a[]){
+Add o=new Add(2,3,4);
+System.out.println(o.display());
+}
+}
