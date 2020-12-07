b2aa7e3fa0d2da239c3c35900805c43c3f6c31d0
someshpradeep31 <61006387+someshpradeep31@users.noreply.github.com>
2020-12-07 13:48:38 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,8 @@
+public class JavaConcat {
+    public static void main(String[] args){
+        String a = "w3schools";
+        String b = ".in";
+        String c = a.concat(b);
+        System.out.println(c);
+    }
+}
